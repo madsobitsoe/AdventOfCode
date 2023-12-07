91578b2b9fecc59c5181237c618409841145557a
Mads Obitsø <madsobitsoe@gmail.com>
2023-12-07 08:57:41 +0100

    Prettier solution day7 2023

--- aoc2023/day7/day7.fsx
+++ aoc2023/day7/day7.fsx
@@ -37,58 +37,11 @@ let convert = function
     | 'A' -> 14
     | _ -> failwith "card not recognized"
     
+let convertJoker = function
+    | 11 -> 1
+    | x -> x
 
-let getType card =
-    // let l = explode card
-    let l = card
-    let grouped = List.groupBy id l
-    match List.length grouped with
-        | 1 -> FiveOfAKind
-        | 2 ->
-            // Can be four of a kind or full house
-            if List.exists (fun (_,x) -> List.length x = 4) grouped
-            then FourOfAKind
-            else FullHouse
-        | 3 ->
-            // Can be ThreeOfAKind or two pairs
-            if List.exists (fun (_,x) -> List.length x = 3) grouped
-            then ThreeOfAKind
-            else TwoPairs
-        | 4 -> Pair
-        | 5 -> Highcard
-        | _ -> failwith "oopsie"
-
-data
-|> List.map (fun (h,b) -> (explode >> List.map convert) h,b)
-|> List.map (fun (a,b) -> getType a,a,b)
-|> List.groupBy (fun (t,h,b) -> t)
-|> List.sortByDescending (fun (t,_) -> t)
-|> List.map (fun (t',h') -> t',List.sortBy (fun (t,h,b) ->  h) h')
-|> List.map (fun (_,hs) -> hs)
-|> List.concat
-|> List.mapi (fun i x -> i+1,x)
-|> List.map (fun (r,(_,_,b)) -> (int64 r) * b)
-|> List.sum
-|> printfn "part 1: %A"
-
-
-// Part 2
-let convert2 = function
-    | '2' -> 2
-    | '3' -> 3
-    | '4' -> 4
-    | '5' -> 5
-    | '6' -> 6
-    | '7' -> 7
-    | '8' -> 8
-    | '9' -> 9
-    | 'T' -> 10
-    | 'J' -> 1
-    | 'Q' -> 12
-    | 'K' -> 13
-    | 'A' -> 14
-    | _ -> failwith "card not recognized"
-
+let convert2 = convert >> convertJoker
 
 let replaceJoker (hand:int list) : int list list =
     let cards = [2;3;4;5;6;7;8;9;10;11;12;13;14]
@@ -97,24 +50,24 @@ let replaceJoker (hand:int list) : int list list =
     let newHands = List.map (fun x -> List.map x hand) replacers
     newHands
 
+let rec getType hand =
+    let getHighJokerHandType =
+        replaceJoker
+        >> List.map getType
+        >> List.sort
+        >> List.head 
 
-let rec getType2 l =
-    if List.contains 1 l then
-        replaceJoker l
-        |> List.map getType2
-        |> List.sort
-        |> List.head
+    if List.contains 1 hand then
+        getHighJokerHandType hand
     else
-        let grouped = List.groupBy id l
+        let grouped = List.groupBy id hand
         match List.length grouped with
             | 1 -> FiveOfAKind
-            | 2 ->
-                // Can be four of a kind or full house
+            | 2 -> // Can be four of a kind or full house
                 if List.exists (fun (_,x) -> List.length x = 4) grouped
                 then FourOfAKind
                 else FullHouse
-            | 3 ->
-                // Can be ThreeOfAKind or two pairs
+            | 3 -> // Can be ThreeOfAKind or two pairs
                 if List.exists (fun (_,x) -> List.length x = 3) grouped
                 then ThreeOfAKind
                 else TwoPairs
@@ -122,17 +75,21 @@ let rec getType2 l =
             | 5 -> Highcard
             | _ -> failwith "oopsie"
 
-data
-|> List.map (fun (h,b) -> (explode >> List.map convert2) h,b)
-|> List.map (fun (a,b) -> getType2 a,a,b)
-|> List.groupBy (fun (t,h,b) -> t)
-|> List.sortByDescending (fun (t,_) -> t)
-|> List.map (fun (t',h') -> t',List.sortBy (fun (t,h,b) ->  h) h')
-|> List.map (fun (_,hs) -> hs)
-|> List.concat
-|> List.mapi (fun i x -> i+1,x)
-|> List.map (fun (r,(_,_,b)) -> (int64 r) * b)
-|> List.sum
-|> printfn "part 2: %A"
+let solve converter data =
+    data
+    |> List.map (fun (h,b) -> (explode >> List.map converter) h,b)
+    |> List.map (fun (a,b) -> getType a,a,b)
+    |> List.groupBy (fun (t,_,_) -> t)
+    |> List.sortByDescending fst
+    |> List.map (fun (t',h') -> t',List.sortBy (fun (t,h,b) ->  h) h')
+    |> List.collect snd
+    |> List.mapi (fun i (_,_,b) -> (int64 (i+1)) * b)
+    |> List.sum
 
+
+solve convert data
+|> printfn "Part 1: %A"
+
+solve convert2 data
+|> printfn "part 2: %A"
 
