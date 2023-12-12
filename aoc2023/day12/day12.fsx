afdafd7480e8f0b33ae958fc6af8b6dffe827798
Mads Obitsø <madsobitsoe@gmail.com>
2023-12-12 19:39:55 +0100

    Add day12 part2 solution
    And rewrite part 1 to use memoization

--- aoc2023/day12/day12.fsx
+++ aoc2023/day12/day12.fsx
@@ -1,5 +1,19 @@
 open System.IO
 
+
+let memoize f =
+    let d = new System.Collections.Generic.Dictionary<_,_>()
+    let rec g x y =
+        match d.TryGetValue ((x,y)) with
+            | (true, res) -> res
+            | _ ->
+                let res = f g x y
+                d.Add((x,y), res)
+                res
+    g
+
+
+
 let explode (s:string) : char list = [for c in s do yield c]
 
 let readFile file =
@@ -10,59 +24,9 @@ let readFile file =
     |> List.ofArray
 
 
-let data = readFile "test.txt"
+//let data = readFile "test.txt"
 let data = readFile "input.txt"
 
-let rec replace (s : char list) =
-    match s with
-        | [] -> [[]]
-        | '?'::xs ->
-            let a = List.map (fun x -> '#' :: x) <| replace xs
-            let b = List.map (fun x -> '.' :: x) <| replace xs
-            a @ b
-        | c::xs -> List.map (fun x -> c :: x) <| replace xs
-
-
-
-let rec checkKey key input =
-    if List.length input < key then None
-    else
-        match input with
-            | [] -> None
-            | '.'::xs -> checkKey key xs
-            | _ ->
-                let i' = List.take key input
-                let checksOut = List.forall ((=) '#') i'
-                let sanity =
-                    if List.length input = key then
-                         checksOut 
-                    else
-                        let next = List.skip key input |> List.head
-                        checksOut && (next <> '#')
-                if sanity && checksOut then Some (List.skip key input)
-                else None
-                
-        
-
-let rec isArrangement keys input =
-    match keys with
-        | [] -> List.forall ((<>) '#') input
-        | x::xs ->
-            match checkKey x input with
-                | None -> false
-                | Some input' -> isArrangement xs input'
-
-let findArrangements (inputs,keys) =
-    List.filter (isArrangement keys) inputs
-    |> List.length
-    
-List.map (fun (a,b) -> replace a, b) data
-|> List.map findArrangements
-|> List.sum
-|> printfn "Part 1: %d"
-
-
-// Part 2
 
 let rec joinWithQuestion xs =
     match xs with
@@ -70,15 +34,74 @@ let rec joinWithQuestion xs =
         | x::[] -> x
         | x::xs -> x @ '?':: joinWithQuestion xs
 
-
 let fixData (a,b) =
     (List.replicate 5 >> joinWithQuestion) a, (List.replicate 5 >> List.concat) b
 
+let rec chomp' f n (xs:char list) : char list option =
+    match n with
+        | 0 ->
+            match xs with
+                | [] -> Some xs
+                | '#'::_ -> None
+                | '?'::xs -> Some ('.'::xs)
+                | '.'::xs -> Some xs
+                | _ -> failwith "Unexpected input"
 
-// List.map fixData data
-// |> List.map (fun (a,b) -> replace a, b) 
-// |> List.map findArrangements
-// |> List.map int64
-// |> List.sum
-// |> printfn "Part 2: %A"
+        | n ->
+            match xs with
+                | [] -> None
+                | '#'::xs' -> f (n-1) xs'
+                | '.'::_ -> None
+                | '?'::xs' -> f (n-1) xs'
+                | _ -> failwith "Unexpected input"
+
+
+let chomp = memoize chomp'
+
+let rec chomper' chompFun f  key xs =
+    match xs with
+        | [] -> [None]
+        | '.'::xs' -> f key xs'
+        | '#'::_ -> [chompFun key xs]
+        | '?'::xs' ->
+            let a = chompFun key ('#'::xs')
+            let b = f key xs'
+            a :: b
+        | _ -> failwith "Unexpected input"
+
+let chomper = memoize (chomper' chomp)
+
+let rec superchomp chomperFun f  keys xs : int64 =
+    if List.length xs < List.sum keys then 0L
+    else
+    match keys with
+        | k::ks ->
+            let a = chomperFun k xs
+            let a' = List.filter Option.isSome a |> List.map Option.get
+            List.fold (fun acc x -> acc + f ks x) 0L a'
+        | [] ->
+            if List.forall ((=) '.') xs
+            then 1L
+            else if List.contains ('#') xs
+            then 0L
+            else 1L
+            
+let gigachomp = memoize (superchomp chomper)
+
+let solver df chompFun acc x =
+    let xs,ks = df x
+    let res = chompFun ks xs
+    acc + res
+
+
+let part1Solver = solver id gigachomp
+let part2Solver = solver fixData gigachomp
+
+data
+|> List.fold part1Solver 0L
+|> printfn "Part 1: %A"
+
+data
+|> List.fold part2Solver 0L
+|> printfn "Part 2: %A"
 
