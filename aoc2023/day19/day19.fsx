b55571b73009e53581def2eedcfea29f544f86be
Mads Obitsø <madsobitsoe@gmail.com>
2023-12-19 08:18:51 +0100

    Add beginning of an idea to solve part 2 day 19 2023

--- aoc2023/day19/day19.fsx
+++ aoc2023/day19/day19.fsx
@@ -13,6 +13,8 @@ let readFile file =
 type Item = { x:int64; m:int64; a:int64; s:int64 }
 let defaultItem = { x = 0L; m = 0L; a = 0L; s = 0L }
 
+
+
 let parseItem (i:string) =
     let rec p ii acc =
         match ii with
@@ -32,6 +34,8 @@ let parseCmp (cmp:char) : (int64 -> int64 -> bool) =
     | '<' -> (<)
     | '>' -> (>)
     | _ -> failwith "Unexpected comp!"
+
+
 
 let getIdx idx (r:Item) =
     match idx with
@@ -89,4 +93,64 @@ let sumItem (i:Item) =
 List.filter (fun i -> applyRule rules "in" i = "A") items
 |> List.fold (fun acc x -> acc + sumItem x) 0L
 |> printfn "Part 1: %A"
+
+// Part 2
+
+type IItem = { x:(int64*int64); m:(int64*int64); a:(int64*int64); s:(int64*int64);}
+let defaultIItem = { x = (0L,4000L); m = (0L,4000L); a = (0L,4000L); s = (0L,4000L); }
+
+let splitInterval cmp (l,h) at =
+    match cmp with
+        | '<' ->
+            let low =
+                if at <= l then None
+                else Some (l, at-1L)
+            let high =
+                Some (max at l, h)
+            low,high
+        | '>' ->
+            let low =
+                Some (l, at)
+            let high =
+                if at >= h then None
+                else Some (at+1L,h)
+            low,high
+
+
+
+splitInterval '<' (0L,4000L) 1351L
+splitInterval '>' (0L,4000L) 1351L
+
+let getIdx' idx (r:IItem) =
+    match idx with
+        | 'x' -> r.x
+        | 'm' -> r.m
+        | 'a' -> r.a
+        | 's' -> r.s
+        | _ -> failwith <| sprintf "Unexpected idx: %A" idx
+
+let setIdx' idx (r:IItem) v =
+    match idx with
+        | 'x' -> { r with x = v }
+        | 'm' -> { r with m = v }
+        | 'a' -> { r with a = v }
+        | 's' -> { r with s = v }
+        | _ -> failwith <| sprintf "Unexpected idx: %A" idx
+
+
+let splitIItem (cmp:char) (at:int64) idx iitem =
+    let v = getIdx' idx iitem
+    match splitInterval cmp v at with
+        | None,None -> None,None
+        | Some l,None ->
+            Some (setIdx' idx iitem l),None
+        | None, Some h ->
+            None, Some (setIdx' idx iitem h)
+        | Some l, Some h ->
+            Some (setIdx' idx iitem h), Some (setIdx' idx iitem l)
+
+
+splitIItem '<' 1351L 's' defaultIItem
+
+
 
