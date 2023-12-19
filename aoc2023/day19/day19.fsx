6b932e76f84ae6548b49adc16b5b4ce0d6c16654
Mads Obitsø <madsobitsoe@gmail.com>
2023-12-19 17:19:15 +0100

    So close

--- aoc2023/day19/day19.fsx
+++ aoc2023/day19/day19.fsx
@@ -35,8 +35,6 @@ let parseCmp (cmp:char) : (int64 -> int64 -> bool) =
     | '>' -> (>)
     | _ -> failwith "Unexpected comp!"
 
-
-
 let getIdx idx (r:Item) =
     match idx with
         | 'x' -> r.x
@@ -60,7 +58,6 @@ let parseRule (r:string) =
         let cmp = sr.[1] |> parseCmp
         let v,dst = sr.[2..] |> (fun s -> s.Split ":") |> (fun a -> Array.head a, (Array.tail >> Array.head) a)
         (fun x -> if cmp (idx x) (int64 v) then Some dst else None)
-
         
     let name,rule =
         r.Split "{"
@@ -97,7 +94,7 @@ List.filter (fun i -> applyRule rules "in" i = "A") items
 // Part 2
 
 type IItem = { x:(int64*int64); m:(int64*int64); a:(int64*int64); s:(int64*int64);}
-let defaultIItem = { x = (0L,4000L); m = (0L,4000L); a = (0L,4000L); s = (0L,4000L); }
+let defaultIItem = { x = (1L,4000L); m = (1L,4000L); a = (1L,4000L); s = (1L,4000L); }
 
 let splitInterval cmp (l,h) at =
     match cmp with
@@ -115,11 +112,11 @@ let splitInterval cmp (l,h) at =
                 if at >= h then None
                 else Some (at+1L,h)
             low,high
+        | _ -> failwith <| sprintf "Unexpected cmp: %A" cmp
 
 
-
-splitInterval '<' (0L,4000L) 1351L
-splitInterval '>' (0L,4000L) 1351L
+splitInterval '<' (1L,4000L) 1351L
+splitInterval '>' (1L,4000L) 1351L
 
 let getIdx' idx (r:IItem) =
     match idx with
@@ -140,7 +137,8 @@ let setIdx' idx (r:IItem) v =
 
 let splitIItem (cmp:char) (at:int64) idx iitem =
     let v = getIdx' idx iitem
-    match splitInterval cmp v at with
+    let low,high =
+        match splitInterval cmp v at with
         | None,None -> None,None
         | Some l,None ->
             Some (setIdx' idx iitem l),None
@@ -148,9 +146,104 @@ let splitIItem (cmp:char) (at:int64) idx iitem =
             None, Some (setIdx' idx iitem h)
         | Some l, Some h ->
             Some (setIdx' idx iitem h), Some (setIdx' idx iitem l)
+    // low,high
+    if cmp = '>' then low,high else high,low
+
+//splitIItem '>' 1351L 's' defaultIItem
+
+let parseRule' (r:string) =
+    let rec workFlow rs acc x =
+        match rs with
+            | [] -> failwith "No more rules :("
+            | r::rs ->
+                match r x with
+                    | (Some dst, Some l),(Some dst', Some r) -> (dst,l)::(dst',r)::acc
+                    | (Some dst, Some left),(None, Some right) ->
+                        workFlow rs ((dst,left)::acc) (Some right )
+                    | _ -> failwith <| sprintf "fail: %A" x
+    
+    let parseSubRule (sr:string) : (IItem option -> (string option * IItem option) * (string option * IItem option)) =
+        if (not (sr.Contains ":")) then (fun x -> (Some sr, x), (Some sr, x))
+        else 
+        let idx = sr.[0]
+        let cmp = sr.[1]
+        
+        let v,dst = sr.[2..] |> (fun s -> s.Split ":") |> (fun a -> Array.head a |> int64, (Array.tail >> Array.head) a)
+        let f (x: IItem option) =
+            let l,r =
+                match x with
+                    | None -> None,None
+                    | Some x -> splitIItem cmp v idx x
+            (Some dst, l),(None, r)
+        f
+        
+    let name,rule =
+        r.Split "{"
+        |> (fun x -> Array.head x, (Array.tail >> Array.head) x |> (fun r -> r.Replace( "}", "")))
+        |> (fun (n,r) -> n, r.Split ",")
+    let subRules = Array.map parseSubRule rule |> List.ofArray
+    name,workFlow subRules []
 
 
-splitIItem '<' 1351L 's' defaultIItem
+let rulesp2 = List.map parseRule' rules' |> Map.ofList
+
+
+let r' = Map.find "in" rulesp2
+
+r' (Some defaultIItem) |> List.distinct
+
+
+let rec applyRule' ruleMap (rule:string, item:IItem option) =
+    match Map.tryFind rule ruleMap with
+        | None -> failwith <| sprintf "Rule %s not found!" rule
+        | Some rule -> rule item |> List.distinct |> List.map (fun (a,b) -> a,Some b)
+
+applyRule' rulesp2
+
+let rec applyRules rulesMap acc rules =
+    match rules with
+        | [] -> acc
+        | rules ->
+            let res = List.collect (applyRule' rulesMap) rules
+            let resDone,resToDo = List.partition (fun (n,_) -> n = "A" || n = "R") res
+            printfn "resToDO: \n%A" resToDo
+            printfn "resDone:\n%A" resDone
+            applyRules rulesMap (resDone@acc) resToDo
+    
+
+
+let joinIntervals (l1,h1) (l2,h2) =
+    if h1+1L=l2 || h2+1L=l1 then [(min l1 l2, max h1 h2)]
+    else if h1 < l2 || h2 < l1 then [(l1,h1);(l2,h2)]
+    
+    else [min l1 l2, max h1 h2]
+
+
+let joinItems' ((i1:IItem), (i2:IItem)) =
+    let x = joinIntervals i1.x i2.x
+    let m = joinIntervals i1.m i2.m
+    let a = joinIntervals i1.a i2.a
+    let s = joinIntervals i1.s i2.s
+    x,m,a,s
+
+
+
+
+
+
+let buildILItem (acc:ILItem) (i:IItem) =
+    { acc with x = i.x::acc.x; m = i.m::acc.m; a = i.a::acc.a; s = i.s::acc.s; }
+
+type ILItem = { x:(int64*int64) list; m:(int64*int64) list; a:(int64*int64) list; s:(int64*int64) list;}
+let defaultILItem = { x = []; m = []; a = []; s = []; }    
+
+applyRules rulesp2 [] [("in", Some defaultIItem)]
+|> List.filter (fst >> (=) "R")
+|> List.map snd
+|> List.filter Option.isSome
+|> List.map Option.get
+|> List.fold buildILItem defaultILItem
+
 
 
 
