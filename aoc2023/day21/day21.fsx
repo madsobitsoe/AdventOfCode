873aee3baf7b521ef3ffcda679cd74e3470c6d2f
Mads Obitsø <madsobitsoe@gmail.com>
2023-12-21 07:22:01 +0100

    day 21 2023 part 1 solution

--- aoc2023/day21/day21.fsx
+++ aoc2023/day21/day21.fsx
@@ -8,11 +8,36 @@ let readFile file =
     |> List.concat
     |> Map.ofList
 
-let data' = readFile "test.txt"
+//let data' = readFile "test.txt"
+let data' = readFile "input.txt"
 
-let start = Map.filter (fun _ v -> v = 'S') data' |> Map.toList |> List.head
+let start = Map.filter (fun _ v -> v = 'S') data' |> Map.toList |> List.map fst |> set 
 
 let data =
     data'
-    |> Map.filter (fun _ v -> v = '.')
+    |> Map.filter (fun _ v -> v = '.' || v = 'S')
 
+
+let step data (i,j) =
+    let neighbors =
+        [i-1,j;
+         i+1,j;
+         i,j-1;
+         i,j+1] |> List.filter (fun (i,j) -> Map.containsKey (i,j) data)
+    set neighbors
+
+
+let steps data ss =
+    Set.fold (fun acc x -> acc + (step data x)) (set []) ss
+
+let rec nSteps data n ss =
+    match n with
+        | 0 -> ss
+        | n ->
+            steps data ss
+            |> nSteps data (n-1)
+
+
+nSteps data 64 start
+|> Set.count
+|> printfn "Part 1: %d"
