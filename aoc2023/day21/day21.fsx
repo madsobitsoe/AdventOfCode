6fdf9a598faff9123f3ca91421f5a301a4a0f5de
Mads Obitsø <madsobitsoe@gmail.com>
2023-12-27 10:53:02 +0100

    Day 21 still does not work

--- aoc2023/day21/day21.fsx
+++ aoc2023/day21/day21.fsx
@@ -11,8 +11,10 @@ let dataToMap data =
     |> List.concat
     |> Map.ofList
 
-//let data'' = readFile "test.txt"
-let data'' = readFile "input.txt"
+
+let data'' = readFile "test.txt"
+//let data'' = readFile "test2.txt"
+//let data'' = readFile "input.txt"
 let data' = dataToMap data''
 
 let maxX = List.length data''
@@ -20,12 +22,7 @@ let maxY = List.head data'' |> List.length
 
 let start = Map.filter (fun _ v -> v = 'S') data' |> Map.toList |> List.map fst |> set 
 
-let data =
-    data'
-    |> Map.filter (fun _ v -> v = '.' || v = 'S')
-
-let data2 = Map.map (fun _ v -> if v = 'S' then 1L else 0L) data
-
+let data = data' |> Map.filter (fun _ v -> v = '.' || v = 'S')
 
 let step data (i,j) =
     let neighbors =
@@ -35,7 +32,6 @@ let step data (i,j) =
          i,j+1] |> List.filter (fun (i,j) -> Map.containsKey (i,j) data)
     set neighbors
 
-
 let rec move max i =
     if i < 0 then move max (max + i)
     else if i < max then i
@@ -44,75 +40,12 @@ let rec move max i =
 let remapCoord' maxX maxY (i,j) =
     let i' = move maxX i
     let j' = move maxY j
-    i',j'
+    i', j'
 
 let remapCoord = remapCoord' maxX maxY
 
-
-let rec mapToWorld max i =
-    if i >= 0 && i < max then 0L
-    else if i >= max then 1L + mapToWorld max (max - i)
-    else -1L + mapToWorld max (max + i)
-
-let step' data (i,j) =
-    let neighbors =
-        [i-1,j;
-         i+1,j;
-         i,j-1;
-         i,j+1]
-         |> List.filter (fun (i,j) -> remapCoord (i,j) |> Map.containsKey <| data)
-         |> List.map (fun (i,j) -> remapCoord (i,j),(mapToWorld maxX i, mapToWorld maxY j))
-    set neighbors
-
-// let step' data (i,j) =
-//     let neighbors =
-//         [i-1,j;
-//          i+1,j;
-//          i,j-1;
-//          i,j+1]
-//          |> List.filter (fun (i,j) -> remapCoord (i,j) |> Map.containsKey <| data)
-//          |> List.map (fun (i,j) -> (i,j),1L + (remapCoord (i,j) |> Map.find <| data ))
-//     set neighbors
-
-
 let steps data ss =
     Set.fold (fun acc x -> acc + (step data x)) (set []) ss
-
-let updateMap map ((i,j),(wi,wj)) =
-    match Map.tryFind (i,j) map with
-        | None -> map //Map.add (i',j') 1L map
-        | Some s ->
-            Map.add (i,j) (Set.add (wi,wj) s) map
-
-let removeFromMap map ((i,j),(wi,wj)) =
-    match Map.tryFind (i,j) map with
-        | None -> map
-        | Some s ->
-            Map.add (i,j) (Set.remove (wi,wj) s) map
-
-let removeAllFromMap map (i,j) =
-    match Map.tryFind (i,j) map with
-        | None -> map
-        | Some s ->
-            Map.add (i,j) (set []) map
-
-// let updateMap  map ((i,j),(c:int64)) =
-//     let i',j' = remapCoord (i,j)
-//     match Map.tryFind (i',j') map with
-//         | None -> map //Map.add (i',j') 1L map
-//         | Some c' ->
-//             Map.add (i',j') (c'+c) map
-        
-let steps' data ss =
-    // printfn "ss: %A" ss
-    // Probably here, use a map not a set
-    // let sss = List.fold (fun acc x -> acc @ (step' data x |> Set.toList)) [] (ss |> Set.toList)
-    let steps = Set.fold (fun acc x -> acc + step' data x) (set []) ss
-
-    // printfn "Steps: %A" steps
-    // printfn "sss: %A" sss
-    let data' = Set.fold (fun acc x -> removeAllFromMap acc x) data ss
-    Set.fold (fun acc x -> updateMap acc x) data' steps
 
 
 let rec nSteps data n ss =
@@ -122,26 +55,94 @@ let rec nSteps data n ss =
             steps data ss
             |> nSteps data (n-1)
 
+nSteps data 64 start
+|> Set.count
+|> printfn "Part 1: %d"
+
+
+// Part 2
+
+let data2 =
+    data
+    |> Map.map (fun _ v -> if v = 'S' then 1L else 0L) 
+    |> Map.map (fun _ v -> if v = 1L then set [(0L,0L)] else set []) 
+
+let movep2 max i (wi:int64) =
+    if i < 0 then max - 1, wi - 1L
+    else if i >= max then 0, wi+1L
+    else i,wi
+
+let remapP2' maxX maxY ((i,j),(wi:int64,wj:int64)) =
+    let i',wi' = movep2 maxX i wi
+    let j',wj' = movep2 maxY j wj
+    ((i',j'),(wi',wj'))
+
+let remapP2 = remapP2' maxX maxY
+
+
+
+let step' (data: Map<(int*int),Set<(int64*int64)>>) (i,j) =
+    let neighbors =
+        [i-1,j;
+         i+1,j;
+         i,j-1;
+         i,j+1]
+         |> List.filter (fun (i,j) -> remapCoord (i,j) |> Map.containsKey <| data)
+
+    let oldWorlds = Map.find (i,j) data
+    let withWorlds (i,j) =
+        oldWorlds
+        |> Set.fold (fun acc x -> (remapP2 ((i,j),x) |> snd) :: acc) []
+        |> set
+        |> (fun x -> (i,j),x)
+    let res = List.map withWorlds neighbors
+    set res
+
+// steps' data2 (set [(1,1)])
+
+
+
+// let updateMap map ((i,j),(wi,wj)) =
+//     match Map.tryFind (i,j) map with
+//         | None -> map
+//         | Some s ->
+//             Map.add (i,j) (Set.add (wi,wj) s) map
+
+let updateMap map ((i,j),(worlds:Set<int64*int64>)) =
+    match Map.tryFind (i,j) map with
+        | None -> map
+        | Some s ->
+            printfn "Found %A at (%d,%d), adding %A" s i j worlds
+            Map.add (i,j) (worlds + s) map
+
+let removeAllFromMap map (i,j) =
+    match Map.tryFind (i,j) map with
+        | None -> map
+        | Some s ->
+            Map.add (i,j) (set []) map
+
+// For each field with step on
+// Step in all possible directions in all worlds
+// Remove old step
+
+let steps' data ss =
+    let steps = Set.fold (fun acc x -> acc + step' data x) (set []) ss
+    printfn "steps: %A" steps
+    let data' = Set.fold (fun acc x -> removeAllFromMap acc x) data ss
+    Set.fold (fun acc x -> updateMap acc x) data' steps
+
+
 let rec nSteps' data n ss =
     match n with
         | 0 -> data
         | n ->
             let map' = steps' data ss
             let ss' = Map.filter (fun _ v -> v <> Set.empty) map' |> Map.toList |> List.map fst |> set
-            // printfn "ss': %A" ss'
             nSteps' map' (n-1) ss'
 
 
-let data3 =
-    Map.map (fun _ v -> if v = 1L then set [(0L,0L)] else set []) data2
 
-
-nSteps data 64 start
-|> Set.count
-|> printfn "Part 1: %d"
-
-
-nSteps' data3 10 start
+nSteps' data2 10 start
 |> Map.map (fun _ v -> Set.count v)
 |> Map.toList
 |> List.map snd
