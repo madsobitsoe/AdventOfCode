c8f907a1944aa8d612be445488d242e75d9e7bd8
Mads Obitsø <madsobitsoe@gmail.com>
2024-12-29 15:41:37 +0100

    Add day 12 2024 solution

--- aoc2024/fsharp/day12/day12.fsx
+++ aoc2024/fsharp/day12/day12.fsx
@@ -1,10 +1,10 @@
+#time
 open System.IO
 
 let explode s = [for c in s do yield c]
 
 let readFile file =
-    File.ReadAllText file
-    |> (fun s -> s.Split "\n")
+    File.ReadAllLines file
     |> List.ofArray
     |> List.map explode
     |> List.filter ((<>) [])
@@ -19,7 +19,7 @@ let neighbors (i,j) =
      (i+1,j);
      (i,j-1);
      (i,j+1);
-        ]
+     ]
 
 let rec buildRegion garden toVisit visited currentLabel region =
     match toVisit with
@@ -56,11 +56,14 @@ let rec buildRegions garden toVisit visited regions =
                         let visited',region = buildRegion garden [x] visited label Set.empty
                         buildRegions garden xs (Set.union region visited) ((label,region)::regions)
 
-
+// let data = readFile "testE.txt" |> toMap
+// let data = readFile "testAbba.txt" |> toMap                        
 //let data = readFile "test.txt" |> toMap
 let data = readFile "input.txt" |> toMap
-let allPositions = Map.keys data |> List.ofSeq
-let regions = buildRegions data allPositions Set.empty []
+
+let regions =
+    let allPositions = Map.keys data |> List.ofSeq
+    buildRegions data allPositions Set.empty []
 
 
 let getFence region plot =
@@ -75,3 +78,32 @@ List.map snd regions
 |> List.map getPrice
 |> List.sum
 |> printfn "Part 1: %d"
+
+
+let countCornersForPoint region (i,j) =
+    let nw = [i-1,j; i,j-1]
+    let nwDiag = i-1,j-1
+    let ne = [i-1,j; i,j+1]
+    let neDiag = i-1,j+1
+    let sw = [i,j-1; i+1,j]
+    let swDiag = i+1,j-1
+    let se = [i,j+1; i+1,j]
+    let seDiag = i+1,j+1
+    let exteriorCorners =
+        [nw;ne;sw;se]
+        |> List.filter (fun x -> List.forall (fun x' -> not (Set.contains x' region)) x)
+        |> List.length
+    let isInteriorCorner (l,d) = List.forall (fun x -> Set.contains x region) l && not (Set.contains d region)
+    let interiorCorners =
+        [nw,nwDiag; ne,neDiag; sw,swDiag; se,seDiag]
+        |> List.filter isInteriorCorner
+        |> List.length
+    exteriorCorners + interiorCorners
+
+let countCornersForRegion region =
+    Set.fold (fun acc x -> acc + countCornersForPoint region x) 0 region
+    
+regions
+|> List.map (fun (label,region) -> countCornersForRegion region * Set.count region)
+|> List.sum
+|> printfn "Part 2: %d"
